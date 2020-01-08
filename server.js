97c8806fc9a5b9db48002a9544f26256fe54e9d3
sesino8 <tonibermejovalenzuela@gmail.com>
2020-01-08 16:25:31 +0100

    Angel mola6

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
 const app = express();
-const PORT = process.env.PORT || 80;
+const PORT = process.env.PORT || 5000;
 
 // Nos conectaremos a la base de datos
 const dbConfig = require('./config/database.config');
